da4746bd20d9dcb03a5f225ef2d0547f84916a48
Администратор <@MANAGER->
2017-10-13 17:31:24 +0500

    Добавьте файлы проекта.

---
+++ SocialNetWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialNetWork.Startup))]
+namespace SocialNetWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
